1793770e02295db1f090a39470c442d7cc73f92c
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2025-03-14 03:15:50 +0700

    init

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToCard from './AddToCard';
+//import styles from './ProductCard.module.css';
+
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddToCard/>
+    </div>
+  )
+}
+
+export default ProductCard
