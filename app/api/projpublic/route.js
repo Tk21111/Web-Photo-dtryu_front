e5674be3cd18f7e4ead2b79155bef2afb20738b5
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2025-03-24 22:45:13 +0700

    fix

--- app/api/projpublic/route.js
+++ app/api/projpublic/route.js
@@ -3,12 +3,12 @@ import Drive from "../../model/Drive"
 import { connectToDatabase } from "../../lib/mongodb";
 import { headers } from "next/headers";
 
-export async function GET(req) {
+export async function GET() {
     await connectToDatabase();
     const projs = await Drive.find({ public: true });
     const headersList = await headers()
 
     console.log(headersList.get('x-forwarded-for'))
-    
+
     return NextResponse.json(projs);
 }
